897e5c7638c32f0c0d445cd873502d4e7f52e15b
ARYA SHARMA <aryarana344@gmail.com>
2022-01-08 00:04:06 +0530

    initial commit

---
+++ AtoZ.java
@@ -0,0 +1,10 @@
+package Practice;
+
+public class AtoZ {
+
+    public static void main(String[] args) {
+        for(char i='a';i<='z';i++){
+            System.out.print(i+ " ");
+        }
+    }
+}
